2758c5d5573e5da48722ae180ea2bd0b12a84fbe
Rech99 <rech_99@hotmail.com>
2021-05-09 16:29:32 -0700

    first

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr
 import time 
 import webbrowser
+import playsound
+import os
+import random
+from gtts import gTTS
 from time import ctime
 
 r = sr.Recognizer()
@@ -8,44 +12,48 @@ r = sr.Recognizer()
 def record_audio(ask = False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            alexa_speak(ask)
         audio =r.listen(source)
         voice_data=''
         try:
-            voice_data = r.recognize_google(audio)
+            voice_data = r.recognize_google(audio, language="en-US")
 
         except sr.UnknownValueError:
-            print('Lo siento, no te entendi')
+            alexa_speak('Lo siento, no te entendi')
         except sr.RequestError:
-            print('Lo siento, error de conexion')
+            alexa_speak('Lo siento, error de conexion')
         return voice_data
 
+
+def alexa_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='es')
+    r = random.randint(1, 10000000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+
 def respond(voice_data):
-    if 'como te llamas' in voice_data:
-        print('Mi nombre es alexis')
-    if 'hora' in voice_data:
-        print(ctime())
-    if 'buscar' in voice_data:
+    if 'Hola' in voice_data:
+        alexa_speak('Hola, mi nombre es alexis')
+    
+    if 'search' in voice_data:
         buscar = record_audio('¿Que necesitas hacer?')
-        url = 'https://google.com/search?q=' + buscar
+        url = 'https://www.youtube.com/results?search_query=' + buscar
         webbrowser.get().open(url)
-        print('Es lo que encontre para:' + buscar)
-    if 'place' in voice_data:
-        lugar = record_audio("¿Que lugar?")
-        url = 'https://google.nl/maps/place/' + lugar + '/&amp;'
-        webbrowser.get().open(url)
-        print ('Esto es lo que encontre sobre:' + lugar)
-    if 'movie' in voice_data:
-        buscar = record_audio('¿Que pelicual quieres buscar?')
-        url = 'https://google.com/search?q=' + buscar
-        webbrowser.get().open(url)
-    if 'actor' in voice_data:
-        buscar = record_audio('¿Que actor quieres buscar?')
-        url = 'https://google.com/search?q=' + buscar
-        webbrowser.get().open(url)
+        alexa_speak('Es lo que encontre para:' + buscar)
+
+    
+
+    if 'good bye' in voice_data:
+        exit()
 
 time.sleep(1)
-print ('¿Como te puedo ayudar?')
-voice_data = record_audio()
-respond(voice_data)
+alexa_speak ('¿Como te puedo ayudar?')
+
+while 1:
+    voice_data = record_audio()
+    respond(voice_data)
 
